f1391cf3cd6bb73c889dca663e6552de31d755ed
Dom <dominic.dipasquale@gmail.com>
2023-04-18 16:30:06 -0700

    try to include data in repo

--- preboot.py
+++ preboot.py
@@ -1,7 +1,7 @@
 import nltk
 # Download the necessary resources
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('vader_lexicon')
-nltk.download('maxent_ne_chunker')
-nltk.download('words')
+nltk.download('punkt', './nltk_data')
+nltk.download('averaged_perceptron_tagger', './nltk_data')
+nltk.download('vader_lexicon', './nltk_data')
+nltk.download('maxent_ne_chunker', './nltk_data')
+nltk.download('words', './nltk_data')
